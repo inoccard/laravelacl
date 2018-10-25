47d7e0cf152407918e669da417aac975b44e0e82
Inoccard <inocenciocardoso19@gmail.com>
2018-10-25 13:14:52 -0300

    laravelacl

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Notice;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,9 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Notice $notice)
     {
+        $notice = 
         return view('home');
     }
 }
