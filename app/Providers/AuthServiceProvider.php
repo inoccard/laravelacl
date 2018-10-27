f96f6425852840681df904739dac3ac8775c1468
inoccard <inocenciocardoso19@gmail.com>
2018-10-27 15:16:07 -0300

    Aumenticação dos posts

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Post;
+use App\USer;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,18 +16,18 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Post::class => \App\Policies\PoliticaPost::class,
     ];
-
     /**
      * Register any authentication / authorization services.
      *
      * @return void
      */
-    public function boot()
+    public function boot( GateContract $gate)
     {
-        $this->registerPolicies();
-
-        //
+        $this->registerPolicies($gate);
+        /*$gate->define('update-post',function(User $user, Post $post){
+            return $user->id == $post->user_id;
+        }); */
     }
 }
