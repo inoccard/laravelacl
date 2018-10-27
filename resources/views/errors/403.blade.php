cc3f69171af6fea77ef203f8acc60eb4aa804078
inoccard <inocenciocardoso19@gmail.com>
2018-10-27 19:41:41 -0300

    Arquivo de acesso negado

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>Acesso Negado!</h1>
+                <h5>Você não tem permissão para acessar esta página!</h5>
+                <h4>Voltar para a <a style="text-decoration:none " href="http://127.0.0.1:8000">página inicial</a></h4>
+            </div>
+        </div>
+    </div>
+@endsection
