884871aecfa4c25953b4d33aee9d31fe20dbd09c
inoccard <inocenciocardoso19@gmail.com>
2018-10-28 13:24:49 -0300

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 <div class="container">
     @forelse($posts as $post)
-        @can('view_post',$post)
+        @can('view_post', $post)
             <h1>{{$post->title}}</h1>
-            <p>{{$post->description}}</p>
+            <p>{{$post->description}}</p><br>
             <b>Autor: {{$post->user->name}}</b>
-            <a href="{{url("/post/$post->id/update")}}">Editar</a>
+            <a style="text-decoration:none" href="{{url("/post/$post->id/update")}}">Editar</a>
+            <hr>
         @endcan
     @empty
         <p>Nenhum post cadastrado!</p>
