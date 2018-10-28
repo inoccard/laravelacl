7416d53a079bcde1d2ff2bfa9064e4876996efa4
inoccard <inocenciocardoso19@gmail.com>
2018-10-28 13:23:37 -0300

    Erro 404 (para páginas não encontradas)

---
+++ resources/views/auth/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>Página não encontrada</h1>
+                <h4><a href="http://127.0.0.1:8000">Voltar para a página inicial</a></h4>
+            </div>
+        </div>
+    </div>
+@endsection
