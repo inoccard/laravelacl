b5c64430ca303a8cb4b5c5722b46ebe5ab01cf6e
inoccard <inocenciocardoso19@gmail.com>
2018-10-28 13:25:20 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/post/{id}/update','HomeController@update');
+Route::get('/roles-permissions','HomeController@rolesPermissions');
