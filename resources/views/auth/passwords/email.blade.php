b72c7fe8f67bb2a67cc40ae18435f3f2a7119634
Inoccard <inocenciocardoso19@gmail.com>
2018-12-11 10:18:56 -0200

    email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,30 @@
-@extends('layouts.app')
+@extends('auth.templates.templateform')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Redefinir Senha') }}</div>
+@section('content-form')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="form-control">
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+</div>
+<form class="login form" method="POST" action="{{url('/password/email')}}">
+    @csrf
+    <div class="form-group{{$errors->has('email') ? 'has-error' : ''}} row">
+        <div class="col-md-12">
+            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+            @if ($errors->has('email'))
+                <span class="help-block" role="alert">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
-</div>
+
+    <div class="form-group">
+        <button type="submit" class="btn btn-login">
+        <i class="fa fa-btn fa-envelope"></i> Enviar link de recuperação</button>
+    </div>
+</form>
 @endsection
