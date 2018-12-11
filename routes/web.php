5e0d388c32cb276106898955f839fad4e4d7372d
Inoccard <inocenciocardoso19@gmail.com>
2018-12-11 10:29:17 -0200

    rotas

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::group(['prefix'=> 'painel'], function(){
+
+    //PostController
+
+    //RoleController
+
+    //PainelController
+    Route::get('/','Painel\PainelController@index');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/post/{id}/update','HomeController@update');
-
-// para debugar permissões do usuário
-Route::get('/roles-permissions','HomeController@rolesPermissions');
+Route::get('/', 'SiteController@index');
