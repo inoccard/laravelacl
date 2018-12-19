4490f6290fb65089ae1e32acb52e51a8874a4397
Inoccard <inocenciocardoso19@gmail.com>
2018-12-19 13:45:00 -0200

    rotas
    criei rotas para post, role, permissions, user

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::group(['prefix'=> 'painel'], function(){
 
     //PostController
-
+    Route::get('posts','Painel\PostController@index');
+    
     //RoleController
+    Route::get('roles','Painel\RoleController@index');
+    Route::get('role/{id}/permissions','Painel\RoleController@permissions');
 
     //PainelController
     Route::get('/','Painel\PainelController@index');
+    
+    //PermissionsController
+    Route::get('permissions','Painel\PermissionController@index');
+
+    //UserController
+    Route::get('users','Painel\UserController@index');
 });
 
 Auth::routes();
 
-Route::get('/', 'SiteController@index');
+Route::get('/', 'Portal\SiteController@index');
