2ecffd5f7f33e14b0d2004264967292d31f7251a
Inoccard <inocenciocardoso19@gmail.com>
2018-12-19 14:09:41 -0200

    index permissions

--- resources/views/painel/permissions/index.blade.php
+++ resources/views/painel/permissions/index.blade.php
@@ -32,10 +32,10 @@
 				<td>{{$permission->nome}}</td>
 				<td>{{$permission->label}}</td>
 				<td>
-					<a href="{{url('/painel/permission/$permission->id/edit')}}" class="edit">
+					<a href="{{url("/painel/permission/$permission->id/edit")}}" class="edit">
 						<i class="fa fa-pencil-square-o"></i>
 					</a>
-					<a href="{{url('/painel/permission/$permission->id/delete')}}" class="delete">
+					<a href="{{url("/painel/permission/$permission->id/delete")}}" class="delete">
 						<i class="fa fa-trash"></i>
 					</a>
 				</td>
