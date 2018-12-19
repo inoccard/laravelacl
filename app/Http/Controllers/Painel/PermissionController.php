a207168d9ec0c08678f7e6d5f0a3fc670d341fa7
Inoccard <inocenciocardoso19@gmail.com>
2018-12-19 13:32:30 -0200

    Controlador de permissões

--- app/Http/Controllers/Painel/PermissionController.php
+++ app/Http/Controllers/Painel/PermissionController.php
@@ -5,8 +5,18 @@ namespace App\Http\Controllers\Painel;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Permission;
 
 class PermissionController extends Controller
 {
-    //
+    private $permission;
+    public function __construct(Permission $permission)
+    {
+        $this->permission = $permission;
+    }
+    public function index()
+    {   
+        $permissions = $this->permission->all();
+        return view('painel.permissions.index', compact('permissions'));
+    }
 }
