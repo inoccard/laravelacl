e665a799ddb4e11e5d55321829bfdbad23763c85
Inoccard <inocenciocardoso19@gmail.com>
2018-12-19 13:44:06 -0200

    update post

--- resources/views/post-update.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1>{{$post->title}}</h1>
-    <p>{{$post->description}}</p>
-    <b>Autor: {{$post->user->name}}</b>
-</div>
-@endsection
