5ed326a2065f4c8e9e737748121f840f9a8438ca
Inoccard <inocenciocardoso19@gmail.com>
2018-12-19 13:40:59 -0200

    Controlador de funções do usuário

--- app/Http/Controllers/Painel/RoleController.php
+++ app/Http/Controllers/Painel/RoleController.php
@@ -5,8 +5,30 @@ namespace App\Http\Controllers\Painel;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Role;
 
 class RoleController extends Controller
 {
-    //
+    private $role;
+
+    public function __construct(Role $role)
+    {
+        $this->role = $role;
+    }
+
+    public function index()
+    {   
+        $roles = $this->role->all();
+        return view('painel.roles.index', compact('roles'));
+    }
+
+    public function permissions($id)
+    {   
+        // Recupera role
+        $role = $this->role->find($id);
+
+        // REcuperar permissões
+        $permissions = $role->permissions(  );
+        return view('painel.roles.permissions', compact('role','permissions'));
+    }
 }
