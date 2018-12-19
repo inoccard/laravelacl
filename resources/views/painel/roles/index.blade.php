a424bf6bfb81fdfc85050dc5a1dc1519cb1f3731
Inoccard <inocenciocardoso19@gmail.com>
2018-12-19 13:43:11 -0200

    index roles

---
+++ resources/views/painel/roles/index.blade.php
@@ -0,0 +1,58 @@
+@extends('painel.templates.template')
+
+@section('content')
+
+	<!--Filters and actions-->
+	<div class="actions">
+			<div class="container">
+				<a class="add" href="forms">
+					<i class="fa fa-plus-circle"></i>
+				</a>
+
+				<form class="form-search form form-inline">
+					<input type="text" name="pesquisar" placeholder="Pesquisar?" class="form-control">
+					<input type="submit" name="pesquisar" value="Encontrar" class="btn btn-success">
+				</form>
+			</div>
+		</div><!--Actions-->
+
+		<div class="clear"></div>
+
+	<div class="container">
+		<h1 class="title">Listagem de Funções</h1>
+
+		<table class="table table-hover">
+		<tr>
+			<th>ID</th>
+			<th>Nome</th>
+			<th>Label</th>
+			<th width="150px">Ações</th>
+		</tr>
+		@forelse($roles as $role)
+			<tr>
+				<td>{{$role->id}}</td>
+				<td>{{$role->nome}}</td>
+				<td>{{$role->label}}</td>
+				<td>
+					<a href='{{url("/painel/roles/$role->id/permissions")}}' class="edit">
+						<i class="fa fa-pencil-square-o"></i>
+					</a>
+					<a href='{{url("/painel/roles/$role->id/edit")}}' class="edit">
+						<i class="fa fa-pencil-square-o"></i>
+					</a>
+					<a href='{{url("/painel/roles/$role->id/delete")}}' class="delete">
+						<i class="fa fa-trash"></i>
+					</a>
+				</td>
+			</tr>
+		@empty
+			<tr>
+				<td colspan="90">
+					<p>Nenhum resultado!</p>
+				</td>
+			</tr>
+		@endforelse	
+		</table>
+	</div>
+
+@endsection
