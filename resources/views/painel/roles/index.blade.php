d487dbf7ab8a43b1c95954819a464c99ca73ec10
Inoccard <inocenciocardoso19@gmail.com>
2018-12-20 15:06:40 -0200

    index roles page

--- resources/views/painel/roles/index.blade.php
+++ resources/views/painel/roles/index.blade.php
@@ -34,8 +34,8 @@
 				<td>{{$role->nome}}</td>
 				<td>{{$role->label}}</td>
 				<td>
-					<a href="{{url("/painel/roles/$role->id/permissions")}}" class="edit">
-						<i class="fa fa-pencil-square-o"></i>
+					<a href="{{url("/painel/role/$role->id/permissions")}}" class="permission">
+						<i class="fa fa-lock"></i>
 					</a>
 					<a href="{{url("/painel/roles/$role->id/edit")}}" class="edit">
 						<i class="fa fa-pencil-square-o"></i>
