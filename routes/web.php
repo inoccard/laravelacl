f8cd89ab6108139957d2b99fdc0bb41ed2de59a5
Inoccard <inocenciocardoso19@gmail.com>
2018-12-20 15:07:15 -0200

    web rotas
    adicionei rotas para id/permissions, id/roles

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,14 @@ Route::group(['prefix'=> 'painel'], function(){
     
     //RoleController
     Route::get('roles','Painel\RoleController@index');
-    Route::get('roles/{id}/permissions','Painel\RoleController@permissions');
+    Route::get('role/{id}/permissions','Painel\RoleController@permissions');
 
     //PainelController
     Route::get('/','Painel\PainelController@index');
     
     //PermissionsController
     Route::get('permissions','Painel\PermissionController@index');
+    Route::get('permission/{id}/roles','Painel\PermissionController@roles');
 
     //UserController
     Route::get('users','Painel\UserController@index');
