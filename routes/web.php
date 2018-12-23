d5b2f37694a5883da8e63d7ad54d2b2817d95283
inoccard <inocenciocardoso19@gmail.com>
2018-12-23 03:59:13 -0200

    rotas

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,21 @@ Route::group(['prefix'=> 'painel'], function(){
 
     //PostController
     Route::get('posts','Painel\PostController@index');
-    
+
     //RoleController
     Route::get('roles','Painel\RoleController@index');
     Route::get('role/{id}/permissions','Painel\RoleController@permissions');
 
     //PainelController
     Route::get('/','Painel\PainelController@index');
-    
+
     //PermissionsController
     Route::get('permissions','Painel\PermissionController@index');
     Route::get('permission/{id}/roles','Painel\PermissionController@roles');
 
     //UserController
     Route::get('users','Painel\UserController@index');
+    Route::get('user/{id}/roles','Painel\UserController@roles');
 });
 
 Auth::routes();
